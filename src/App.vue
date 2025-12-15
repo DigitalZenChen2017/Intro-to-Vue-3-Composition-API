16248ab470f5ce9eea5b9444be825a8067b49577
boris-chen <theboris.chen@gmail.com>
2025-12-15 12:48:19 -0500

    added product description

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const productTitle = ref("Socks");
+const productDescription = ref("A pair of warm, fuzzy socks.");
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ productTitle }}</h1>
+        <p>{{ productDescription }}</p>
       </div>
     </div>
   </div>
