29ca8190a32c59e003447da775cd75bed2866335
boris-chen <theboris.chen@gmail.com>
2025-12-29 18:37:16 -0500

    added v if for inStock

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+const inStock = ref(true);
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
       </div>
     </div>
   </div>
