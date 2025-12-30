c1537c8cebd39c89a4592937172079fe66bf2d35
boris-chen <theboris.chen@gmail.com>
2025-12-29 20:18:28 -0500

    finished L3 lesson

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
+import sockGreenImage from "./assets/images/socks_green.jpeg";
+import sockBlueImage from "./assets/images/socks_blue.jpeg";
 
-const product = ref('Socks')
-
+const product = ref("Socks");
+const greenSockImage = ref(sockGreenImage);
+const blueSockImage = ref(sockBlueImage);
+const greenSockLink = ref("https://example.com/green-socks");
+const blueSockLink = ref("https://example.com/blue-socks");
 </script>
 
 <template>
@@ -10,6 +15,12 @@ const product = ref('Socks')
     <div class="product-container">
       <div class="product-image">
         <!-- image goes here -->
+        <img :src="greenSockImage" alt="Green Socks" />
+        <a :href="greenSockLink" target="_blank"
+          >Purchase Green Christmasy Socks Here!</a
+        >
+        <img :src="blueSockImage" alt="Blue Socks" />
+        <a :href="blueSockLink" target="_blank">Buy Navy Manly Socks Here!</a>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
